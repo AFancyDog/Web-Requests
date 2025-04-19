62eb3e02db463448678905f9c8f8504d4f723048
AFancyDog <afancydog@rtfs.de>
2025-04-19 22:15:31 +0100

    Now to add actual code

---
+++ src/client/java/afd/webrequests/WebRequestsClient.java
@@ -0,0 +1,10 @@
+package afd.webrequests;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WebRequestsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
